fbdfb8b7d900cb7e82755a34bde1870e743a7840
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-10-06 17:58:24 +0530

    Version Should be used in case it is given in Config,

--- bootloader.js
+++ bootloader.js
@@ -397,7 +397,7 @@
       if(!config.resourceUrl && foo.location){
         config.resourceUrl = foo.location.origin;
       }
-			config.version = (config.debug === false && config.version)?  config.version: "" ;
+			config.version = (config.debug === false || config.version)?  config.version: "" ;
       if(!foo.is.Value(config.livereloadUrl) && foo.URL){
         var url = new foo.URL(config.resourceUrl);
         config.livereloadUrl = url.protocol + "//"+url.hostname + ":35729/livereload.js"
